a24661916ad9acea6511c1746b0f2bbed2b79dfa
John Rees <john@bitsushi.com>
2016-05-26 00:21:10 +0100

    FPS counter

--- src/ts/states/game.ts
+++ src/ts/states/game.ts
@@ -5,8 +5,13 @@ export class Game extends Phaser.State {
   player: Player;
 
   create() {
+    this.game.time.advancedTiming = true;
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
     this.player = new Player(this.game, this.world.centerX, this.world.centerY);
+  }
+
+  render() {
+    this.game.debug.text(this.game.time.fps.toString(), 10, 20, "#ffffff");
   }
 
 }
